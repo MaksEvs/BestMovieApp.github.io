8a7b76d3ebdeb4ec0271ad03979ad7f23e4b0d21
Maks <maksim.evseev666@gmail.com>
2024-04-29 00:34:00 +0300

    fix height favorite card

--- src/components/FavoritePage/FavoriteFilmItem.js
+++ src/components/FavoritePage/FavoriteFilmItem.js
@@ -1,21 +1,24 @@
 import { useGetMovieByIdQuery } from "../../store/apiSlice";
 import Loader from "../MainPage/MovieList/Loader/Loader";
 import { Link } from "react-router-dom";
-import './FavoriteFilmItem.css'
+import "./FavoriteFilmItem.css";
 
 const FavoriteFilmItem = ({ filmID }) => {
 	const { data, error, isLoading, isFetching } = useGetMovieByIdQuery(filmID);
 
-	console.log(filmID);
 	return (
-		<li className= 'favorite-link'>
+		<li className="favorite-link">
 			{" "}
 			{isLoading ? (
 				<Loader />
 			) : (
 				<Link to={`/film/${data.kinopoiskId}`}>
 					<li className="favorite-item" key={data.kinopoiskId}>
-						<img className="favorite-img" src={data.posterUrlPreview} alt={data.nameRu} />
+						<img
+							className="favorite-img"
+							src={data.posterUrlPreview}
+							alt={data.nameRu}
+						/>
 						<p className="favorite-title">{data.nameRu}</p>
 					</li>
 				</Link>
